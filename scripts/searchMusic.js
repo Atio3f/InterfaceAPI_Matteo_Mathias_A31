df7845eb2faa3a21e268c9f1d0380dd86acfaca3
Matteo <matteomathian@gmail.com>
2025-03-17 15:08:49 +0100

    resolution de bug pour obtenir données

--- scripts/searchMusic.js
+++ scripts/searchMusic.js
@@ -1,17 +1,14 @@
 const accessToken = 'hssnQuuDbZj5QUuW4ooRb7ZRX7230tPiKQ-Xj2IBGuKRW0XnU30zEgYexIpXNk_T'; 
 
 async function rechercheDeMusique(nomMusique) {
-    const url = `https://api.genius.com/search?q=$` + nomMusique + `&access_token=` + accessToken;
+    const url = `https://api.genius.com/search?q=${nomMusique}&access_token=${accessToken}`;
 
     const reponse = await fetch(url, {
         method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        }
     });
-
     const donneesMusique = await reponse.json();
     console.log(donneesMusique);
-}
+    return donneesMusique;
+} 
+rechercheDeMusique('Hello');
 
-rechercheDeMusique('Hello Adele');
