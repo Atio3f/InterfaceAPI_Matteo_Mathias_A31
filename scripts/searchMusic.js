44050a6b6c2f52b3a3ef6887400a117ddb43a893
Matteo <matteomathian@gmail.com>
2025-03-20 10:30:30 +0100

    Changement de la methode search music pour faire la recherche en fonction du nom de la musique

--- scripts/searchMusic.js
+++ scripts/searchMusic.js
@@ -1,6 +1,9 @@
 const accessToken = 'hssnQuuDbZj5QUuW4ooRb7ZRX7230tPiKQ-Xj2IBGuKRW0XnU30zEgYexIpXNk_T'; 
 
-async function rechercheDeMusique(nomMusique) {
+
+async function rechercheDeMusique() {
+    const nomMusique = document.getElementById("utilisateurInput").value;
+   
     const url = `https://api.genius.com/search?q=${nomMusique}&access_token=${accessToken}`;
 
     const reponse = await fetch(url, {
@@ -10,5 +13,3 @@ async function rechercheDeMusique(nomMusique) {
     console.log(donneesMusique);
     return donneesMusique;
 } 
-rechercheDeMusique('Hello');
-
