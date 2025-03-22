c9f72a47b2ba04ca2d427eb6efcf7683ab588e03
Mattéo <matteo.banroques--mathian@etu.univ-grenoble-alpes.fr>
2025-03-22 14:16:02 +0100

    Affichage des resultats de la recherche de musique.

--- scripts/searchMusic.js
+++ scripts/searchMusic.js
@@ -2,7 +2,7 @@ const accessToken = 'hssnQuuDbZj5QUuW4ooRb7ZRX7230tPiKQ-Xj2IBGuKRW0XnU30zEgYexIp
 
 
 async function rechercheDeMusique() {
-    const nomMusique = "for";//document.getElementById("utilisateurInput").value;
+    const nomMusique = document.getElementById("utilisateurInput").value;
     if(nomMusique != ""){
         const url = `https://api.genius.com/search?q=${nomMusique}&access_token=${accessToken}`;
 
@@ -12,11 +12,29 @@ async function rechercheDeMusique() {
         const donneesMusique = await reponse.json();
         const musique = donneesMusique.response.hits[0].result;
         console.log(donneesMusique);
-        ajoutFavori(musique);
-        //return donneesMusique;
+        console.log(musique);
+        console.log(musique.full_title);
+        let resultatDiv = document.getElementById('main');
+        resultatDiv.innerHTML = "";
+            donneesMusique.response.hits.forEach(hit => {
+            const musique = hit.result;
+            const musiqueHTML = `
+                <div class="musique">
+                    <img src="${musique.song_art_image_url}" alt="${musique.full_title}" width="100">
+                    <h3>${musique.full_title}</h3>
+                    <p>Artiste : ${musique.artist_names}</p>
+                    <a href="${musique.url}" target="_blank">Voir sur Genius</a>
+                </div>
+            `;
+
+            resultatDiv.innerHTML += musiqueHTML;
+        });
+        
+       
+        
     }else{
         console.log("error");
-        //return "error";
+        
     }
     
 } 
